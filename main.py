0a583fd60b9cdc1f54c8c97e7b8245435e1bfd0f
IS250 Student <changeme@example.com>
2025-11-17 13:02:30 -0600

    added pseudocode

--- main.py
+++ main.py
@@ -17,22 +17,10 @@ exam average project
 
 
 # Your Python code begins below this line.
-# Your Python code begins below this line.
 #define the function
-def calculate_average():
-  #prompt user to enter score 1
-  score1=int(input("Enter the first score: "))
-  #prompt the user to enter score 2
-  score2=int(input("Enter the second score: "))
-  #prompt the user to enter score 3
-  score3=int(input("Enter the third score: "))
-  #calculate the average of the 3 scores
-  average= (score1+score2+score3)/3
-  #print the average of the 3 scores
-  print("First score:",score1,"\n""Second score:",score2,"\n","Third Score",score3,"\n","The average score is:",average,)
+
 
 # Call your function when your program is ready
 calculate_average()
-# Call your function when your program is ready
-calculate_average()   
+
   
